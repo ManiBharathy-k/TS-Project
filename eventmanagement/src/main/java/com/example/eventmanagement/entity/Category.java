c885dcbf021e2b76778c9b42b6b211b59f684b09
ManiBharathy-k <manibharathy362@gmail.com>
2024-11-01 12:47:09 +0530

    third commit

--- eventmanagement/src/main/java/com/example/eventmanagement/entity/Category.java
+++ eventmanagement/src/main/java/com/example/eventmanagement/entity/Category.java
@@ -1,5 +1,43 @@
 package com.example.eventmanagement.entity;
 
 public class Category {
-
+ private Long id;
+ private String name;
+ private String descrpition;
+ private Long event_id;
+ 
+public Category() {
+	super();
+}
+public Category(Long id,String name, String descrpition, Long event_id) {
+	super();
+	this.id= id;
+	this.name = name;
+	this.descrpition = descrpition;
+	this.event_id = event_id;
+}
+public Long getid() {
+	return id;
+}
+public String getName() {
+	return name;
+}
+public void setid(Long id) {
+	this.id= id;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescrpition() {
+	return descrpition;
+}
+public void setDescrpition(String descrpition) {
+	this.descrpition = descrpition;
+}
+public Long getEvent_id() {
+	return event_id;
+}
+public void setEvent_id(Long event_id) {
+	this.event_id = event_id;
+}
 }
--- eventmanagement/src/main/java/com/example/eventmanagement/entity/Location.java
+++ eventmanagement/src/main/java/com/example/eventmanagement/entity/Location.java
@@ -1,5 +1,7 @@
 package com.example.eventmanagement.entity;
 
 public class Location {
+	private Long id;
+	private String address;
 
 }
